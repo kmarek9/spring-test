fd7363ec91845bc678bab82bbd9111c55dd8cbe7
kmarek9 <37239801+kmarek9@users.noreply.github.com>
2023-09-23 13:12:16 +0200

    Update LoanCalculator.java

--- src/main/java/com/example/demo/LoanCalculator.java
+++ src/main/java/com/example/demo/LoanCalculator.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoanCalculator {
     public static double calculateFixedInstallment(double loanAmount, double oprocentowanie, int liczbaRat) {
-        double q = 1 + (oprocentowanie / 12);
-        return (loanAmount * oprocentowanie) / (12 * (1 - Math.pow(q, -liczbaRat)));
+        double qPerMonth = 1 + (oprocentowanie / 12);
+        return (loanAmount * oprocentowanie) / (12 * (1 - Math.pow(qPerMonth, -liczbaRat)));
     }
 }
