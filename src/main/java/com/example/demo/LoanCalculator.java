102bac091e736e86038e4c62c4b0609a87bd84d9
Marek Koszałka <kmarek9@gmail.com>
2023-09-23 12:29:26 +0200

    MYP-45 - refactor

--- src/main/java/com/example/demo/LoanCalculator.java
+++ src/main/java/com/example/demo/LoanCalculator.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class LoanCalculator {
-    public static double calculateFixedInstallment(double kwotaKredytu, double oprocentowanie, int liczbaRat) {
+    public static double calculateFixedInstallment(double loanAmount, double oprocentowanie, int liczbaRat) {
         double q = 1 + (oprocentowanie / 12);
-        return (kwotaKredytu * oprocentowanie) / (12 * (1 - Math.pow(q, -liczbaRat)));
+        return (loanAmount * oprocentowanie) / (12 * (1 - Math.pow(q, -liczbaRat)));
     }
 }
